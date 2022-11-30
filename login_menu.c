c04d9c900bc11ab251c9635f800ec5a6fb15d34b
Benno Vasconcellos <bennovasconcellos@gmail.com>
2022-11-30 23:25:57 +0000

    AS

--- login_menu.c
+++ login_menu.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "color.h"
+/* #include "color.h" */
+#define RESET              "\x1b[0m"
+#define BLACK              "\x1b[30m"
+#define RED                "\x1b[31m"
+#define GREEN              "\x1b[32m"
+#define YELLOW             "\x1b[33m"
+#define BLUE               "\x1b[34m"
+#define MAGENTA            "\x1b[35m"
+#define CYAN               "\x1b[36m"
+#define WHITE              "\x1b[37m"
+#define BACKGROUND_BLACK   "\x1b[40m"
+#define BACKGROUND_RED     "\x1b[41m"
+#define BACKGROUND_GREEN   "\x1b[42m"
+#define BACKGROUND_YELLOW  "\x1b[43m"
+#define BACKGROUND_BLUE    "\x1b[44m"
+#define BACKGROUND_MAGENTA "\x1b[45m"
+#define BACKGROUND_CYAN    "\x1b[46m"
+#define BACKGROUND_WHITE   "\x1b[47m"
+#define BOLD         "\x1b[1m"
+#define ITALIC       "\x1b[3m"
+#define UNDERLINE    "\x1b[4m"
+
 
 typedef struct{
     int id_permission, enable;
@@ -40,7 +61,7 @@ void main(){
         scanf("%d", &opcao);
         switch(opcao){
         case 0:
-            printf("Saindo do programa...\n");
+            printf(RED"Saindo do programa...\n"RESET);
             break;
         case 1:
             printf("Login");
