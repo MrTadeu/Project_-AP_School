5f17e3a7f8d96038ef32d2cd1da3a8b91be69d5e
Benno Vasconcellos <bennovasconcellos@gmail.com>
2022-12-03 18:42:09 +0000

    asd

---
+++ colorprint.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+char* replaceWord(const char* s, const char* oldW, const char* newW){ 
+    char* result; 
+    int i, cnt = 0; 
+    int newWlen = strlen(newW); 
+    int oldWlen = strlen(oldW); 
+  
+    // Counting the number of times old word 
+    // occur in the string 
+    for (i = 0; s[i] != '\0'; i++) { 
+        if (strstr(&s[i], oldW) == &s[i]) { 
+            cnt++;
+            // Jumping to index after the old word. 
+            i += oldWlen - 1; 
+        } 
+    }
+    // Making new string of enough length 
+    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1); 
+  
+    i = 0; 
+    while (*s) { 
+        // compare the substring with the result 
+        if (strstr(s, oldW) == s) { 
+            strcpy(&result[i], newW); 
+            i += newWlen; 
+            s += oldWlen; 
+        } 
+        else
+            result[i++] = *s++; 
+    }
+    result[i] = '\0'; 
+    return result; 
+}
+
+/* void main(){
+    char str[] = "This <red>is</red> a test string";
+    char red[] = "\033[0;31m";
+    char reset[] = "\033[0m";
+    char* result = replaceWord(str, "<red>", red);
+    result = replaceWord(result, "</red>", reset);
+    printf("%s", result);
+} */
+char* printf_color(char* str, char* color){ 
+    char* result = (char*)malloc(strlen(str) + strlen(color) + 1); 
+
+}
