e1034b0098e25d848fb13802e2d2412c98b92cd8
GamerPedro4K <pedro.t.carvalho12345@gmail.com>
2022-12-05 17:27:37 +0000

    dd

---
+++ All_functions/validators.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+int isdigit_cheker(char *str){
+    int i;
+    for(i = 0; i < strlen(str); i++){
+        if(isdigit(str[i])){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
+char *lowercase(char *str){
+    int i;
+    for(i = 0; i < strlen(str); i++){
+        str[i] = tolower(str[i]);
+    }
+    return str;
+}
+
+char *uppercase(char *str){
+    int i;
+    for(i = 0; i < strlen(str); i++){
+        str[i] = toupper(str[i]);
+    }
+    return str;
+}
