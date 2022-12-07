0bf206a6b10d8355d8cfc27656c41edc1345ee58
Benno Vasconcellos <bennovasconcellos@gmail.com>
2022-12-07 09:29:29 +0000

    blablablabla

--- readWrite/permissions.c
+++ readWrite/permissions.c
@@ -1,16 +1,21 @@
 
 typedef struct{
-    int id_permission, enable;
+    int lerAlunos, escreverAlunos, listarAlunos, deletarAlunos, lerCursos, escreverCursos, listarCursos, deletarCursos, lerRoles, escreverRoles, listarRoles, deletarRoles;
 }hasPermission;
 
+/* typedef struct{
+    rolesID registo, listarAlunos, deletarAlunos;
+}prmissionsTipe; */
+typedef struct{    
+    int id;
+    char *name;
+    hasPermission permission;
+}role;
 typedef struct {
     char *nome;
     int id_role, ano, id, id_course;
 }Aluno;
-typedef struct{
-    rolesID registo, listarAlunos, deletarAlunos;
-}prmissionsTipe;
 
-void prmissions(){
+void permissions(){
     
 }
