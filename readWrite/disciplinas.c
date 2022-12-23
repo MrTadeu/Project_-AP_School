d5b7737a8fb70da7d9678908d7dbeda16e44cce9
j-falcao2002 <joaorpalharesfalcao@gmail.com>
2022-12-23 20:24:40 +0000

    la

--- readWrite/disciplinas.c
+++ readWrite/disciplinas.c
@@ -3,23 +3,25 @@
 #include <string.h>
 #include "../All_functions\global.h"
 
-courseStruct *getAllCourses(AlunoFileStruct *alunosFile, int n_alunos, int *n_courses){
+courseStruct *readBinCourses(int *n_courses){
     courseStruct *courses = malloc(sizeof(courseStruct));
-    for (int i = 0; i < n_alunos; i++){
-        int found = 0;
-        for (int j = 0; j < *n_courses; j++){
-            if (strcmp(alunosFile[i].course, courses[j].name) == 0){
-                found = 1;
-                break;
-            }
-        }
-        if (!found){
-            courses = realloc(courses, ((*n_courses)+1)*sizeof(courseStruct));
-            courses[*n_courses].id = *n_courses+1;
-            courses[*n_courses].name = malloc((strlen(alunosFile[i].course)+1));
-            strcpy(courses[*n_courses].name, alunosFile[i].course);
-            *n_courses=*n_courses+1;
-        }
-    } 
+    int i;
+    FILE *file = fopen("data/bin/courses.bin","rb");
+    if (!file) {
+        printc("\n\n\tImpossivel abrir Ficheiro [red]course.bin[/red]\n\n");
+        exit(1);
+    }
+
+    for (i = 0;; i++){
+        courses = realloc(courses, (i+1)*sizeof(courseStruct));
+        if(fread(&courses[i].id, sizeof(int), 1, file) != 1) break;
+
+        size_t courseLen;
+        fread(&courseLen, sizeof(size_t), 1, file);
+        courses[i].name = malloc(courseLen);
+        fread(courses[i].name, courseLen, 1, file);
+    }
+    *n_courses = i-1;
+    fclose(file);
     return courses;
 }
