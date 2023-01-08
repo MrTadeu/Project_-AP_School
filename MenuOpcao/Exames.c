2fb49a45aa758f4a30485dd05699eea4d8836066
Benno Vasconcellos <bennovasconcellos@gmail.com>
2023-01-08 19:38:50 +0000

    dasd

--- MenuOpcao/Exames.c
+++ MenuOpcao/Exames.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../All_functions/global.h"
 
-//void menu()
 
-typedef struct{
-    int dia, mes, ano, hora, minuto;
+/* typedef struct{
+    int dia, mes, hora, minuto;
 }tempoExames;
 
 typedef struct{
     int disciplina, professor, sala, regime;
     tempoExames data;
-}Exames;
+}Exames; */
 
+Exames* exame;
+int n_exames = 0;
+
+void criarExame(){
+    exame = malloc(sizeof(Exames));
+    printf("Para qual disciplina pretende criar um exame? ");
+    scanf("%d", &exame[n_exames].disciplina);
+    exame = realloc(exame, sizeof(Exames)*(n_exames+1));
+
+    printf("Pra qual sala pretende criar um exame? ");
+
+
+    n_exames++;
+}
