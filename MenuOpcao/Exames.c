566b3339bbff8633c212c7200e2060b5a33a3c30
Benno Vasconcellos <bennovasconcellos@gmail.com>
2023-01-08 19:27:17 +0000

    asd

--- MenuOpcao/Exames.c
+++ MenuOpcao/Exames.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 
 //void menu()
+
+typedef struct{
+    int dia, mes, ano, hora, minuto;
+}tempoExames;
+
+typedef struct{
+    int disciplina, professor, sala, regime;
+    tempoExames data;
+}Exames;
+
