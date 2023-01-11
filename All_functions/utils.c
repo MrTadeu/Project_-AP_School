347628bbedd62c277ddc6a894b8609b4ee9ee67f
Benno Vasconcellos <bennovasconcellos@gmail.com>
2023-01-11 09:37:57 +0000

    gfd

--- All_functions/utils.c
+++ All_functions/utils.c
@@ -78,12 +78,15 @@ void ValidarZeroUm(int num){
 void timeNow(){
     time_t now;
     time(&now);
+    struct tm *tempoAgora;
+
     tempoAgora = localtime(&now);
 
     //printf("Current date and time: %04d-%02d-%02d %02d:%02d:%02d\n", tempoAgora->tm_year + 1900, tempoAgora->tm_mon + 1, tempoAgora->tm_mday, tempoAgora->tm_hour, tempoAgora->tm_min, tempoAgora->tm_sec);
 }
 
 void diferencaTempo(int ano, int mes, int dia, int hora, int minuto){
+    struct tm *tempoAgora;
     timeNow();
     tempo.anoAtual= tempoAgora->tm_year +  1900;
     tempo.mesAtual = tempoAgora->tm_mon + 1;
@@ -98,9 +101,9 @@ void diferencaTempo(int ano, int mes, int dia, int hora, int minuto){
     tempo.difMinuto = tempo.minutoAtual - minuto;
 }
 
-int TempoCerto(int ano, int mes, int dia, int hora, int minuto){
+/* int TempoCerto(int ano, int mes, int dia, int hora, int minuto){
 
-}
+} */
 /* int generate_unique_numbers(int *numbers, int size) {
   // seed the random number generator
   srand(time(NULL));
